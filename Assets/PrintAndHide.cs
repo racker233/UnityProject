83de97849ca00ddefb9a42a5caa3400faf8636f0
Zhenghao Ruan <1754824991@qq.com>
2024-08-29 02:36:54 +1000

    Added logic to deactivate Red objects and disable Blue objects' Renderer

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -1,17 +1,42 @@
 using UnityEngine;
+
 public class PrintAndHide : MonoBehaviour
 {
     private int i = 3;
     public Renderer rend;
+    private int blueDeactivateThreshold;
 
     void Start()
     {
         i++;
+        if (gameObject.tag == "Blue")
+        {
+            blueDeactivateThreshold = Random.Range(150, 251); // 生成 150 到 250 之间的随机整数
+        }
     }
 
     void Update()
     {
         Debug.Log(gameObject.name + ":" + i);
         i++;
+
+        if (gameObject.tag == "Red" && i == 100)
+        {
+            gameObject.SetActive(false); // 停用红色游戏对象
+        }
+
+        if (gameObject.tag == "Blue")
+        {
+            if (i == blueDeactivateThreshold)
+            {
+                rend.enabled = false; // 在指定的阈值禁用 Renderer 组件
+            }
+            else if (i > blueDeactivateThreshold)
+            {
+                rend.enabled = true; // 在超过阈值后重新启用 Renderer 组件
+            }
+        }
     }
 }
+
+
