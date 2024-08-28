cdb3d7af97ee45c50345634d1e4a618faf912ab1
Zhenghao Ruan <1754824991@qq.com>
2024-08-29 02:11:08 +1000

    60 percent complete

--- Assets/LoadAssets.cs
+++ Assets/LoadAssets.cs
@@ -1,18 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class LoadAssets : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        public GameObject redObj;
-    }
+    public GameObject redObj;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
